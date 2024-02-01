a5be2537961ec40bd98e040a86253019fea58a2f
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-01 19:18:26 +0900

    rename: 파일 rename
    post -> create-post
    board -> posts

--- routes/board.js
+++ routes/board.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 // 게시판 목록 html 렌더링
 router.get('/', (req, res) => {
-    res.render('board/board');
+    res.render('board/posts');
 });
 
-router.get('/post', (req, res) => {
-    res.render('board/post');
+router.get('/create', (req, res) => {
+    res.render('board/create-post');
 });
 
 module.exports = router;
