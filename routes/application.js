85f39616b975445503f6e1a753dd661f2be38c2d
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-01 17:02:02 +0900

    feat: 지원성공 페이지 라우팅

--- routes/application.js
+++ routes/application.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+// application html 페이지 렌더링
 router.get('/', (req, res) => {
     res.render('application');
 });
+
+// 지원 성공 시 html 페이지 렌더링
+
+router.get('/success', (req, res) => {
+    res.render('success_apply')
+});
+
+
 
 module.exports = router;
