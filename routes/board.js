d96aac0fcf920b95401a8f853036d3ae8f5dcf23
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-11 20:07:46 +0900

    delete: 엔드포인트 제거

--- routes/board.js
+++ routes/board.js
@@ -47,25 +47,4 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// 게시물 생성 페이지 렌더링
-router.get('/upload', (req, res) => {
-    res.render('board/create-post');
-});
-
-// 새로운 메시지 등록 API
-router.post('/', (req, res) => {
-    const message = req.body;
-
-    db.query('INSERT INTO qna SET ?', message, (err, result) => {
-        if (err) {
-            console.error('MySQL query error:', err);
-
-            // TODO: 실패 페이지 만들기
-            res.render('board/create-post');
-        } else {
-            res.render('board/posts', { result })
-        }
-    });
-});
-
 module.exports = router;
