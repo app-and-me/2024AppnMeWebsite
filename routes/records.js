80df6e4f86488337c11f18082af5367ae8c227ea
hyeg0121 <w2218@e-mirim.hs.kr>
2024-02-16 20:50:50 +0900

    feat: 카테고리 클릭하면 해당 페이지로 이동, 렌더링

--- routes/records.js
+++ routes/records.js
@@ -5,4 +5,20 @@ router.get('/', (req, res) => {
     res.render('records/records');
 });
 
+router.get('/11/developers', (req, res) => {
+    res.render('records/dev11');
+});
+
+router.get('/11/designers', (req, res) => {
+    res.render('records/design11');
+});
+
+router.get('/12/developers', (req, res) => {
+    res.render('records/dev12');
+});
+
+router.get('/12/designers', (req, res) => {
+    res.render('records/design12');
+});
+
 module.exports = router;
