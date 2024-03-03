d7469f560cd051a092ac52b3eac1b3b4e1d7b183
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-03 10:54:10 +0900

    feat: 에러 페이지 추가

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const db = require('./database/database');
 
 // body-parser 설정
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // 라우터 추가
 const membersRouter = require('./routes/members');          // 부원 소개
@@ -25,23 +25,28 @@ const applicationRouter = require('./routes/application');  // 지원하기
 const boardRouter = require('./routes/board');              // QnA
 const recordsRouter = require('./routes/records');          // 활동 기록
 const uploadRouter = require('./routes/upload');            // QnA 업로드
-const studentsRouter = require('./routes/students');   
-const messagesRouter = require('./routes/messages');     
+const studentsRouter = require('./routes/students');
+const messagesRouter = require('./routes/messages');
 const studentsListRouter = require('./routes/students-list');
 app.use('/members', membersRouter);
 app.use('/application', applicationRouter);
 app.use('/board', boardRouter);
 app.use('/records', recordsRouter);
 app.use('/upload', uploadRouter);
-app.use('/api/students', studentsRouter);   
+app.use('/api/students', studentsRouter);
 app.use('/api/messages', messagesRouter);
 app.use('/api/students-list', studentsListRouter);
 
+// 404
+app.get('/404', (req, res) => {
+    res.status(404).render('error404', {error: {code: 404, message: '요청한 페이지를 찾을 수 없어요.'}});
+})
+
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
 });
 
 // 서버 시작
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+    console.log(`http://localhost:${port}`);
 });
