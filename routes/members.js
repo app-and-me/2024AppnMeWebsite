90ce1760a2431b311f3aaa3fbceebb7ba9213833
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-03 13:15:07 +0900

    feat: json 데이터 읽고, ejs 렌더링

--- routes/members.js
+++ routes/members.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const fs = require('fs');
 
+// memberData.json 파일의 경로
+const memberDataPath = path.join(__dirname, 'data.json');
+
+// 멤버 정보를 렌더링
 router.get('/', (req, res) => {
-    res.render('members/members');
+    // memberData.json 파일을 읽어옴
+    fs.readFile(memberDataPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading memberData.json:', err);
+            res.status(500).render('error404', {error: {code: 500, message: '서버가 응답하지 않습니다.'}});
+            return;
+        }
+
+        try {
+            // JSON 파싱
+            const memberData = JSON.parse(data);
+            console.log(memberData)
+            console.log({ members: memberData.members11 })
+            // 멤버 정보를 사용하여 members.ejs 렌더링
+            res.render('members/members', { members: memberData.members11 });
+        } catch (jsonError) {
+            console.error('Error parsing JSON:', jsonError);
+            res.status(500).render('error404', {error: {code: 500, message: '서버가 응답하지 않습니다.'}});
+        }
+    });
 });
 
 module.exports = router;
