45847ca8c5a0d5a0a448c71b54309f269e557999
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-03 11:10:19 +0900

    feat: 날짜 형식 포맷해서 return

--- routes/board.js
+++ routes/board.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database/database');
 
+// 날짜 형식을 변경하는 함수
+function formatDate(inputDate) {
+    const date = new Date(inputDate);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+}
+
 // 게시판 목록 렌더링
 router.get('/', (req, res) => {
     db.query('SELECT * FROM qna', (err, results) => {
@@ -9,7 +20,13 @@ router.get('/', (req, res) => {
             console.error('MySQL query error:', err);
             res.status(404).render('error404', {error: {code: 404, message: '요청한 페이지를 찾을 수 없어요.'}});
         } else {
-            res.status(200).render('board/posts', { posts: results });
+            const formattedResults = results.map(result => ({
+                ...result,
+                question_date: formatDate(result.question_date)
+            }));
+
+            console.log(formattedResults)
+            res.status(200).render('board/posts', { posts: formattedResults });
         }
     });
 });
@@ -30,6 +47,8 @@ router.get('/:id', (req, res) => {
                 } else {
                     const post = posts[0];
                     const postWithReplies = { ...post, replies };
+
+                    postWithReplies.question_date = formatDate(postWithReplies.question_date);
 
                     console.log(postWithReplies)
                     res.render('board/post-detail', { post: postWithReplies });
