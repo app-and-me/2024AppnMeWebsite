7a74bba97cbf7c205562964d43a7ef9844186d32
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-03 19:35:21 +0900

    feat: 부원 데이터 json 파일에서 조회 후 렌더링

--- routes/records.js
+++ routes/records.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const fs = require('fs');
+
+// data.json 파일의 경로
+const dataPath = path.join(__dirname, 'data.json');
 
 router.get('/', (req, res) => {
     res.render('records/records');
 });
 
-router.get('/11/developers', (req, res) => {
-    res.render('records/dev11');
-});
+router.get('/detail', (req, res) => {
+    // data.json 파일을 읽어옴
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading data.json:', err);
+            res.status(500).render('error', { error: { code: 500, message: '인터넷 서버 에러가 발생했습니다.' } });
+            return;
+        }
 
-router.get('/11/designers', (req, res) => {
-    res.render('records/design11');
-});
+        try {
+            const jsonData = JSON.parse(data);
 
-router.get('/12/developers', (req, res) => {
-    res.render('records/dev12');
-});
+            const cohort = req.query.cohort || '11';
+            const major = req.query.major || 'dev';
+            const recordsData = jsonData.records[cohort] && jsonData.records[cohort][major] || [];
+            console.log(recordsData);
 
-router.get('/12/designers', (req, res) => {
-    res.render('records/design12');
+            res.render('records/record-details', { records: recordsData});
+        } catch (jsonError) {
+            console.error('Error parsing JSON:', jsonError);
+            res.status(500).render('error', { error: { code: 500, message: '인터넷 서버 에러가 발생했습니다.' } });
+        }
+    });
 });
 
 module.exports = router;
