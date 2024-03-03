ddc936a6105b4f253d821cfe61d5ac247c7a3b2b
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-03 13:29:29 +0900

    feat: 학과에 따라 부원들 데이터 바꾸기

--- routes/members.js
+++ routes/members.js
@@ -3,29 +3,31 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 
-// memberData.json 파일의 경로
 const memberDataPath = path.join(__dirname, 'data.json');
 
-// 멤버 정보를 렌더링
 router.get('/', (req, res) => {
-    // memberData.json 파일을 읽어옴
     fs.readFile(memberDataPath, 'utf8', (err, data) => {
         if (err) {
-            console.error('Error reading memberData.json:', err);
-            res.status(500).render('error404', {error: {code: 500, message: '서버가 응답하지 않습니다.'}});
+            console.error('Error reading data.json:', err);
+            res.status(500).render('error404', { error: { code: 500, message: '인터넷 서버 에러가 발생했습니다.' } });
             return;
         }
 
         try {
-            // JSON 파싱
             const memberData = JSON.parse(data);
-            console.log(memberData)
-            console.log({ members: memberData.members11 })
-            // 멤버 정보를 사용하여 members.ejs 렌더링
-            res.render('members/members', { members: memberData.members11 });
+
+            // 기수 및 전공 쿼리스트링 파라미터 가져오기
+            const cohort = req.query.cohort || 'cohort11'; // 디폴트는 cohort11
+            const major = req.query.major || 'software';  // 디폴트는 software
+
+            // 선택된 기수 및 전공에 해당하는 데이터 가져오기
+            const selectedData = memberData[cohort] && memberData[cohort][major] || [];
+
+            // 렌더링
+            res.render('members/members', { members: selectedData, cohort, major });
         } catch (jsonError) {
             console.error('Error parsing JSON:', jsonError);
-            res.status(500).render('error404', {error: {code: 500, message: '서버가 응답하지 않습니다.'}});
+            res.status(500).render('error404', { error: { code: 500, message: '인터넷 서버 에러가 발생했습니다.' } });
         }
     });
 });
